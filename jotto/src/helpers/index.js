38667a3f0087133f81f9c23e7c0e2a5e372e4c4e
aalsberge <aalsberge@homeaway.com>
2021-01-21 12:56:23 -0600

    added helper file and test for checking letter count

---
+++ jotto/src/helpers/index.js
@@ -0,0 +1,12 @@
+/**
+ * @method getLetterMatchCount
+ * @param {string} guessedWord - guessed word
+ * @param {string} secretWord - secret word
+ * @returns {number} - numbers of letters matching the secret word
+ */
+
+export function getLetterMatchCount(guessedWord, secretWord) {
+    const secretWordSet = new Set(secretWord.split(''));
+    const guessedWordSet = new Set(guessedWord.split(''));
+    return  [...secretWordSet].filter( letter => guessedWordSet.has(letter)).length;
+}
